d7fa838a9cd4210a2b62db3dc465556b38b94434
Qwqovoewe <qq1729524191@yeah.net>
2024-09-07 11:12:06 +0800

    Initial commit

---
+++ src/main/java/com/wrx/my_music_platform/MyMusicPlatformApplication.java
@@ -0,0 +1,13 @@
+package com.wrx.my_music_platform;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyMusicPlatformApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MyMusicPlatformApplication.class, args);
+    }
+
+}
